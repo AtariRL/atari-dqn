4e2347ac36af8668f62e4a839d233ed33d85a897
Peter K <peter@servon.eu>
2021-12-08 10:01:11 +0100

    rfix

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import subprocess
 
-subprocess.run("python irb1000.py &; python irb10000.py &; python irb50000.py &; python irb100000.py &", shell=True)
+subprocess.run("python irb1000.py & python irb10000.py & python irb50000.py & python irb100000.py", shell=True)
