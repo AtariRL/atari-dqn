cd453dc164e7b05118d13367c9b8280e2c3c1b35
Peter K <peter@servon.eu>
2021-12-09 09:39:46 +0100

    Added training

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import subprocess
 
-subprocess.run("python /home/atari/atari-dqn/irb1000.py & python /home/atari/atari-dqn/irb10000.py & python /home/atari/atari-dqn/irb50000.py & python /home/atari/atari-dqn/irb100000.py", shell=True)
-# & python /home/atari/atari-dqn/pushfreq50.py & python /home/atari/atari-dqn/pushfreq100.py & python /home/atari/atari-dqn/pushfreq200.py & python /home/atari/atari-dqn/pushfreq500.py & python /home/atari/atari-dqn/updatefreq50.py & python /home/atari/atari-dqn/updatefreq100.py & python /home/atari/atari-dqn/updatefreq200.py & python /home/atari/atari-dqn/updatefreq500.py
+#subprocess.run("python /home/atari/atari-dqn/irb1000.py & python /home/atari/atari-dqn/irb10000.py & python /home/atari/atari-dqn/irb50000.py & python /home/atari/atari-dqn/irb100000.py", shell=True)
+subprocess.run("python /home/atari/atari-dqn/pushfreq50.py & python /home/atari/atari-dqn/pushfreq100.py & python /home/atari/atari-dqn/pushfreq200.py & python /home/atari/atari-dqn/pushfreq500.py", shell=True)
+#subprocess.run("python /home/atari/atari-dqn/updatefreq50.py & python /home/atari/atari-dqn/updatefreq100.py & python /home/atari/atari-dqn/updatefreq200.py & python /home/atari/atari-dqn/updatefreq500.py", shell=True)
