d6697e366456d0d0b924e9da802eda69fa5298e9
sava17 <37406543+sava17@users.noreply.github.com>
2021-12-16 11:01:45 +0100

    removed special boi

--- sam.py
+++ sam.py
@@ -12,7 +12,7 @@ subprocess.check_call([sys.executable, '-m', 'pip', 'install',
 # BREAKOUT
 #subprocess.run("python breakout-irb-1000.py & python breakout-irb-10000.py & python breakout-irb-50000.py & python breakout-irb-100000.py", shell=True)
 #subprocess.run("python breakout-pfreq-50.py & python breakout-pfreq-100.py & python breakout-pfreq-200.py & python breakout-pfreq-500.py", shell=True)
-subprocess.run("python breakout-ufreq-50.py & python breakout-ufreq-100.py & python breakout-ufreq-200.py & python breakout-ufreq-500.py & python special-breakout.py & python breakout-pfreq-200.py & python breakout-pfreq-500.py", shell=True)
+subprocess.run("python breakout-ufreq-50.py & python breakout-ufreq-100.py & python breakout-ufreq-200.py & python breakout-ufreq-500.py & python breakout-pfreq-200.py & python breakout-pfreq-500.py", shell=True)
 
 
 #subprocess.run("python assualt_duel_dqn.py & python assualt_duel_dqn_random_irm.py & python assualt_duel_dqn_prio_irm.py & python assualt_duel_dqn_highest_irm.py", shell=True)
