5eba79b0007f07ef413f24ad3e8baf1e8977be0f
sava17 <37406543+sava17@users.noreply.github.com>
2021-12-16 11:31:50 +0100

    ...

--- main_update.py
+++ main_update.py
@@ -5,4 +5,4 @@ subprocess.check_call([sys.executable, '-m', 'pip', 'install',
 'gym==0.19.0'])
 
 # BREAKOUT
-subprocess.run("python /home/atari/atari-dqn/breakout-ufreq-50.py & python /home/atari/atari-dqn/breakout-ufreq-100.py & python /home/atari/atari-dqn/breakout-ufreq-200.py & python /home/atari/atari-dqn/breakout-ufreq-500.py & python /home/atari/atari-dqn/special-breakout.py & python /home/atari/atari-dqn/breakout-pfreq-200.py & python /home/atari/atari-dqn/breakout-pfreq-500.py", shell=True)
+subprocess.run("python /home/atari/atari-dqn/breakout-ufreq-50.py & python /home/atari/atari-dqn/breakout-ufreq-100.py & python /home/atari/atari-dqn/breakout-ufreq-200.py & python /home/atari/atari-dqn/breakout-ufreq-500.py ", shell=True)
