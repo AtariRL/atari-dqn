2457ee9716ba26c92c0c826f6bb1c5cabc7e96b6
Peter K <peter@servon.eu>
2021-12-16 10:26:47 +0100

    path

--- sam.py
+++ sam.py
@@ -1,14 +1,14 @@
 import subprocess
 
 # PONG
-#subprocess.run("python /home/atari/atari-dqn/irb1000.py & python /home/atari/atari-dqn/irb10000.py & python /home/atari/atari-dqn/irb50000.py & python /home/atari/atari-dqn/irb100000.py", shell=True)
-#subprocess.run("python /home/atari/atari-dqn/pushfreq50.py & python /home/atari/atari-dqn/pushfreq100.py & python /home/atari/atari-dqn/pushfreq200.py & python /home/atari/atari-dqn/pushfreq500.py", shell=True)
-#subprocess.run("python /home/atari/atari-dqn/updatefreq50.py & python /home/atari/atari-dqn/updatefreq100.py & python /home/atari/atari-dqn/updatefreq200.py & python /home/atari/atari-dqn/updatefreq500.py", shell=True)
+#subprocess.run("python irb1000.py & python irb10000.py & python irb50000.py & python irb100000.py", shell=True)
+#subprocess.run("python pushfreq50.py & python pushfreq100.py & python pushfreq200.py & python pushfreq500.py", shell=True)
+#subprocess.run("python updatefreq50.py & python updatefreq100.py & python updatefreq200.py & python updatefreq500.py", shell=True)
 
 # BREAKOUT
-#subprocess.run("python /home/atari/atari-dqn/breakout-irb-1000.py & python /home/atari/atari-dqn/breakout-irb-10000.py & python /home/atari/atari-dqn/breakout-irb-50000.py & python /home/atari/atari-dqn/breakout-irb-100000.py", shell=True)
-#subprocess.run("python /home/atari/atari-dqn/breakout-pfreq-50.py & python /home/atari/atari-dqn/breakout-pfreq-100.py & python /home/atari/atari-dqn/breakout-pfreq-200.py & python /home/atari/atari-dqn/breakout-pfreq-500.py", shell=True)
-subprocess.run("python /home/atari/atari-dqn/breakout-ufreq-50.py & python /home/atari/atari-dqn/breakout-ufreq-100.py & python /home/atari/atari-dqn/breakout-ufreq-200.py & python /home/atari/atari-dqn/breakout-ufreq-500.py & python /home/atari/atari-dqn/special-breakout.py & python /home/atari/atari-dqn/breakout-pfreq-200.py & python /home/atari/atari-dqn/breakout-pfreq-500.py", shell=True)
+#subprocess.run("python breakout-irb-1000.py & python breakout-irb-10000.py & python breakout-irb-50000.py & python breakout-irb-100000.py", shell=True)
+#subprocess.run("python breakout-pfreq-50.py & python breakout-pfreq-100.py & python breakout-pfreq-200.py & python breakout-pfreq-500.py", shell=True)
+subprocess.run("python breakout-ufreq-50.py & python breakout-ufreq-100.py & python breakout-ufreq-200.py & python breakout-ufreq-500.py & python special-breakout.py & python breakout-pfreq-200.py & python breakout-pfreq-500.py", shell=True)
 
 
-#subprocess.run("python /home/atari/atari-dqn/assualt_duel_dqn.py & python /home/atari/atari-dqn/assualt_duel_dqn_random_irm.py & python /home/atari/atari-dqn/assualt_duel_dqn_prio_irm.py & python /home/atari/atari-dqn/assualt_duel_dqn_highest_irm.py", shell=True)
+#subprocess.run("python assualt_duel_dqn.py & python assualt_duel_dqn_random_irm.py & python assualt_duel_dqn_prio_irm.py & python assualt_duel_dqn_highest_irm.py", shell=True)
