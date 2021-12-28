5637e2ebe535b003c12c2b4745e0b869e0a68d22
sava17 <37406543+sava17@users.noreply.github.com>
2021-12-28 02:25:21 +0100

    new start files

---
+++ alone_prio_dqn.py
@@ -0,0 +1,7 @@
+import subprocess
+import sys
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 
+'gym==0.19.0'])
+
+subprocess.run("python /home/atari/atari-dqn/prio-dqn.py", shell=True)
